945b60668018ce0cd7361d358e88cf6071709fd1
Dillon Foley <foleydil@gmail.com>
2018-03-19 22:17:36 -0400

    add jQuery, declare initial variable, start makeGrid() func

--- designs.js
+++ designs.js
@@ -1,10 +1,15 @@
 // Select color input
+let color = $('colorPicker');
+
 // Select size input
+let gridWidth = $('#inputWeight');
+let gridHeight = $('#inputHeight');
 
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+//Takes user input for grid dimensions as arguments,
+//and adds table of that size to body of index.html
+function makeGrid(gridHeight, gridWidth) {
+  $('body').append('<table><tr><td></td><td></td><td></td></tr></table>');
 }
+
+// When size is submitted by the user, call makeGrid
+$('#sizePicker').submit(makeGrid());
