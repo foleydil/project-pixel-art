0fc20a873396a91c6acce59e228b231d0a666eef
Dillon Foley <foleydil@gmail.com>
2018-03-22 12:01:51 -0400

    feat: Started makeGrid() function
    TODO: finish function to call makeGrid when form is submitted

--- designs.js
+++ designs.js
@@ -2,13 +2,27 @@
 let color = $('#colorPicker').val();
 
 // Select size input
-let gridWidth = $('#inputWeight').val();
-let gridHeight = $('#inputHeight').val();
+let gridWidth = 0;
+let gridHeight = 0;
 
 //Takes user input for grid dimensions as arguments,
 //and adds table of that size to body of index.html
 function makeGrid(gridHeight, gridWidth) {
-  $('body').append('<table><tr><td></td><td></td><td></td></tr></table>');
+  //establish HTML string to be later appended to body of page
+  //will append appropriate grid HTML based on user input
+  let gridHTML = "";
+
+  //set user input to gridWidth and gridHeight vars
+  $(sizePicker).submit(function() {
+    let gridWidth = $('#inputWeight').val();
+    let gridHeight = $('#inputHeight').val();
+  });
+
+  for (var i = 0; i < gridWidth.length; i++) {
+    array[i]
+  }
+
+  $('body').append(gridHTML);
 }
 
 // When size is submitted by the user, call makeGrid
