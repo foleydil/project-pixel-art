24ce12d9270eeb21ac26b456462508ba399671b1
Dillon Foley <foleydil@gmail.com>
2018-03-26 12:02:32 -0400

    feat: Update td listener to use user-selected color

--- designs.js
+++ designs.js
@@ -1,25 +1,41 @@
+//Variables
+let sizePicker = $('#sizePicker');
+let colorPicker = $('#colorPicker');
+let pixelCanvas = $('#pixelCanvas');
+let color = "#000000";
 
-// When size is submitted by the user, call makeGrid
-$('#sizePicker').submit(function(event) {
-  //set user input to gridCols and gridRows vars
-  //supress default action of form submission, to avoid page reloading
+
+//Event listeners
+// When size is submitted by the user, call makeGrid, passing in user input
+sizePicker.submit(function(event) {
+  //supress default action of form submission, to block page reloading
   event.preventDefault();
+  //set user input to gridCols and gridRows vars
   let gridCols = $('#inputWeight').val();
   let gridRows = $('#inputHeight').val();
   makeGrid(gridRows, gridCols)
 });
 
-// set color var
-  let color = $('#colorPicker').val();
+//update color var when user dismisses color picker
+colorPicker.change(function() {
+  color = colorPicker.val();
+});
 
+//change td color to chosen color assignment
+pixelCanvas.on('click', 'td', function(event) {
+  $(this).css("background-color", color);
+});
+
+
+//Functions
 /*Takes user input for grid dimensions as arguments,
 appends table to body of index.html*/
 function makeGrid(gridRows, gridCols) {
-  //clear existing grid
-  $('#pixelCanvas').empty();
-  //establish string of grid html to later append to body of page
+  //clear any existing grid
+  pixelCanvas.empty();
+  //establish local var (string) to later append to body of page
   let gridHTML = "";
-  //create grid
+  //build HTML string for grid
   for (let r = 0; r < gridRows; r++) {
     gridHTML += "<tr>";
     for (let c = 0; c < gridCols; c++) {
@@ -27,12 +43,6 @@ function makeGrid(gridRows, gridCols) {
     };
     gridHTML += "</tr>\n";
   };
-  //add grid to body of index.html
-  $('#pixelCanvas').append(gridHTML);
+  //add completed grid to body of index.html
+  pixelCanvas.append(gridHTML);
 }
-
-
-//listener for every 'td', change td color to colorPicker assignment
-$("#pixelCanvas").on('click', 'td', function(event) {
-  $(event.target).css("background-color", color);
-});
